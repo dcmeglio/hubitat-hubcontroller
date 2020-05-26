800e887a08f88513627fc3722904370d28b6ed51
dcmeglio <dmeglio@gmail.com>
2020-05-26 10:07:55 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # hubitat-hubcontroller
- 
+ This driver provides access to common hub commands to reboot the hub, restart the Hubitat application, and shutdown the hub.
+
+## Drivers
+Install the __Hub Controler__ driver and create a Virtual Device. 
+
+### Configuration
+The app supports both hubs that use Hub Security and those that do not. If you are using hub security you will need to supply the username and password of an administrative user for your hub. Optionally you can also specify an IP address. If specified this will allow you to perform the commands on a different (remote) hub. Use 127.0.0.1 to refer to the local hub.
+
+Three custom commands are provided, `restart`, `reboot`,  and `shutdown`.
+
+## Donations
+If you find this driver useful, please consider making a [donation](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7LBRPJRLJSDDN&source=url)! 
---
+++ drivers/Hub_Controller.groovy
@@ -0,0 +1,92 @@
+/**
+ *  Hubitat Hub Controller
+ *
+ *  Copyright 2020 Dominick Meglio
+ *
+ */
+ 
+metadata {
+    definition (
+		name: "Hubitat Hub Controller", 
+		namespace: "dcm", 
+		author: "dmeglio@gmail.com",
+		importUrl: "https://raw.githubusercontent.com/dcmeglio/hubitat-hubcontroller/master/drivers/Hub_Controller.groovy"
+	) {
+		capability "Actuator"
+        
+		command "reboot"
+		command "restart"
+		command "shutdown"
+    }
+}
+
+
+preferences {
+	input "heIP", "string", title: "Hub IP Address", defaultValue: "127.0.0.1"
+    input "heSecurity", "bool", title: "Hub security enabled?"
+	input "heUsername", "string", title: "Hub security username"
+	input "hePassword", "password", title: "Hub security password"
+}
+
+def reboot() {
+	callHubEndpoint("/hub/reboot")
+}
+
+def restart() {
+	callHubEndpoint("/hub/restart")
+}
+
+def shutdown() {
+	callHubEndpoint("/hub/shutdown")
+}
+
+def getURL() {
+	if (heIP == "127.0.0.1")
+		return "127.0.0.1:8080"
+	return heIP
+}
+
+def getLoginCookie() {
+	def cookie = ""
+
+    if (heSecurity)
+    {
+		httpPost(
+			[
+				uri: "http://${getURL()}",
+				path: "/login",
+				query: 
+				[
+					loginRedirect: "/"
+				],
+				body:
+				[
+					username: heUsername,
+					password: hePassword,
+					submit: "Login"
+				]
+			]
+		)
+		{ resp ->
+            cookie = resp?.headers?.'Set-Cookie'?.split(';')?.getAt(0)
+        }
+	}
+	return cookie
+}
+
+def callHubEndpoint(endpoint) {
+	def cookie = getLoginCookie()
+	httpPost(
+		[
+			uri: "http://${getURL()}",
+			path: endpoint,
+			headers:
+			[
+				"Cookie": cookie
+			]
+		]
+	) 
+	{
+		resp -> log.debug resp.status
+	}
+}
